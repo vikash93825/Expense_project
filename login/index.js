5bda86f1a6f96f4f5aa0613fb996a93da7044891
vikash93825 <yadav.kk74780@gmail.com>
2020-10-12 22:22:07 +0530

    login

---
+++ login/index.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', function () {
+    var form = document.querySelector('form');
+    form.addEventListener('submit', handleLogin)
+});
+
+
+function handleLogin(event) {
+    event.preventDefault();
+    var form = new FormData(event.target);
+    var email = form.get('email');
+    var password = form.get('password')
+    data = JSON.parse(localStorage.getItem('data'))
+    var flag = 0;
+
+    for (var i = 0; i < data.length; i++) {
+        if (email == data[i]['email'] && password == data[i]['password']) {
+            localStorage.setItem('loginUser',JSON.stringify(data[i]))
+            location.assign('/home/vikash/Expense_Manager/dashboard/dashboard.html')
+            break;
+        }
+        else if (email == data[i]['email']) {
+            if (password != data[i]['password']) {
+                flag = 1
+            }
+        }
+    }
+
+    if (flag == 1) {
+        var show = document.getElementById('show');
+        show.textContent = "*Wrong Password"
+    }
+    else {
+        var show = document.getElementById('show');
+        show.textContent = "!Account doesn't exist"
+    }
+}

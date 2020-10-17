6cd6ed53b4665bc79877976f57e8ad483180cfc6
vikash93825 <yadav.kk74780@gmail.com>
2020-10-17 10:52:56 +0530

    updated

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -9,4 +9,27 @@ window.addEventListener('load',function(){
     else{
 
     }*/
+
+    var form = document.querySelector('form');
+    form.addEventListener('submit',handleDashboard);
 })
+
+
+function handleDashboard(){
+    event.preventDefault();
+    //console.log(event.target)
+    var form = new FormData(event.target);
+    var title = form.get('title');
+    var type = form.get('type')
+    var amount = form.get('amount')
+
+    var tran  = JSON.parse(localStorage.getItem('loginUser'));
+    console.log(tran)
+    tran['transactions'].push({title,type,amount})
+
+    var data = JSON.parse(localStorage.getItem('data'));
+
+    data[data.length-1]['transactions'].push({title,type,amount})
+
+    console.log(data)
+}
